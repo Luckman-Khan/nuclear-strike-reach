29512308af4ad4cb5806bb2ab8d6283dba70cfe8
Luckman <luckmankhan005@gmail.com>
2026-04-08 14:15:07 +0530

    vercel metrics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import 'leaflet/dist/leaflet.css'
 import App from './App'
 import './style.css'
@@ -7,5 +8,6 @@ import './style.css'
 ReactDOM.createRoot(document.getElementById('app') as HTMLElement).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>,
 )
